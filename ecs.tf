8c3fcb8448725152292282faa12e389c53ce42f8
Aamanchi-Varsha <70124628+varshaa-04@users.noreply.github.com>
2025-10-27 20:22:59 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,19 +1,31 @@
-resource "aws_ecs_cluster" "main" {
-  name = "myapp-cluster"
-}
-
+resource "```hcl
 resource "aws_ecs_task_definition" "app_task" {
+  family                  aws_ecs_task_definition" "app_task" {
   family                   = "myapp-task"
-  network_mode             = "awsvpc"
+ = "myapp-task"
+  network_mode              network_mode             = "awsvpc"
+  requires = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  cpu                     _compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = "arn:aws:iam::688412148742:role/ecsTaskExecutionRole"
-  
+  execution_role_arn       = = "256"
+  memory                   = "512"
+  execution_role_arn       = data.aws_iam_role data.aws_iam_role.ecs_execution.arn
 
-  container_definitions    = jsonencode([{
-    name      = "myapp"
-    image = "688412148742.dkr.ecr.us-east-1.amazonaws.com/web-app:latest"
+  container_definitions = jsonencode.ecs_execution.arn
+
+  container_definitions = jsonencode([{
+    name      = "web-app"
+    image     = "688412148742.dkr.ecr.us-east-1.amazonaws.com/web-app:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 5000
+      hostPort      = 5000
+    }]
+ ([{
+    name      = "web-app"
+    image     = "688412148742.dkr.ecr.us-east-1.amazonaws.com/web-app:latest"
     essential = true
     portMappings = [{
       containerPort = 5000
