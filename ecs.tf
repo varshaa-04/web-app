f3569cacdd5a9e4443daaa13338b7af0c995c39c
Aamanchi-Varsha <70124628+varshaa-04@users.noreply.github.com>
2025-10-27 18:39:30 +0530

    Create ecs.tf

---
+++ ecs.tf
@@ -0,0 +1,32 @@
+resource "aws_ecs_cluster" "main" {
+  name = "myapp-cluster"
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "myapp-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  container_definitions    = jsonencode([{
+    name      = "myapp"
+    image     = aws_ecr_repository.myapp_repo.repository_url
+    essential = true
+    portMappings = [{
+      containerPort = 5000
+      hostPort      = 5000
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "app_service" {
+  name            = "myapp-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets         = ["subnet-xxxxxx"] # Replace with actual subnet IDs
+    assign_public_ip = true
+  }
+}
