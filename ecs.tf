4e2c7ec9c95e6ec006d9fa940b493547d1ea0585
Aamanchi-Varsha <70124628+varshaa-04@users.noreply.github.com>
2025-10-27 20:35:14 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,12 +1,34 @@
+resource "aws_ecs_cluster" "main" {
+  name = "myapp-cluster"
+}
+
+resource "aws_iam_role" "ecs_execution" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "app_task" {
   family                   = "myapp-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-
   execution_role_arn       = aws_iam_role.ecs_execution.arn
-  task_role_arn            = aws_iam_role.ecs_execution.arn
 
   container_definitions = jsonencode([{
     name      = "web-app"
@@ -18,5 +40,6 @@ resource "aws_ecs_task_definition" "app_task" {
     }]
   }])
 }
+
 
 
